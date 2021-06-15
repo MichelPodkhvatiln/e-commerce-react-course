5753b61a309bf78de63625cabba6676e71414742
Mikhail_Podkhvatilin <podkhvatilin_my@groupbwt.com>
2021-06-15 15:43:54 +0300

    finished styling our menu-item component

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -6,13 +6,12 @@ const MenuItem = (props) => {
   const { title, imageUrl, size } = props;
 
   return (
-    <div
-      style={{
+    <div className={`menu-item ${size ?? ''}`}>
+      <div className={'background-image'} style={{
         backgroundImage: `url(${imageUrl})`
-      }}
-      className={`menu-item ${size ?? ''}`}>
+      }}/>
       <div className={'content'}>
-        <h1 className={'title'}>{title}</h1>
+        <h1 className={'title'}>{title.toUpperCase()}</h1>
         <span className={'subtitle'}>SHOP NOW</span>
       </div>
     </div>
