8227432d510e117c329363d38d9554478eb7e296
Mikhail_Podkhvatilin <podkhvatilin_my@groupbwt.com>
2021-06-16 09:58:21 +0300

    add sign in form and auth page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Route, Switch } from 'react-router-dom';
 
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
+import Auth from "./pages/auth/auth.component";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Switch>
         <Route exact={true} path={'/'} component={HomePage}/>
         <Route path={'/shop'} component={ShopPage}/>
+        <Route path={'/auth'} component={Auth}/>
       </Switch>
      </div>
   );
